de433bc92c17f613fe45a10e277eabbe7b2eac2b
DIPTIRANJAN PANDA <pandadiptiranjan915@gmail.com>
2026-05-24 20:14:42 +0530

    first commit

---
+++ Last Coin in a Game of Alternates.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int coin(vector<int>& arr) {
+        // code here
+        sort(arr.begin(), arr.end(), [](int &a, int &b){
+            return a>b;
+        });
+        return arr[arr.size()-1];
+    }
+};
