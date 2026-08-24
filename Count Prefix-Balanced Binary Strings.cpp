3669fab0703f27b554f467a5c4dc87afd1eddf7f
DIPTIRANJAN PANDA <pandadiptiranjan915@gmail.com>
2026-08-24 21:12:20 +0530

    first commit

---
+++ Count Prefix-Balanced Binary Strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int prefixStrings(int n) {
+        const long long MOD = 1000000007;
+        
+        long long fact[2005];
+        fact[0] = 1;
+        
+        for (int i = 1; i <= 2 * n; ++i)
+            fact[i] = fact[i - 1] * i % MOD;
+        
+        auto power = [&](long long a, long long b) {
+            long long res = 1;
+            while (b) {
+                if (b & 1) res = res * a % MOD;
+                a = a * a % MOD;
+                b >>= 1;
+            }
+            return res;
+        };
+        
+        long long numerator = fact[2 * n];
+        long long denominator = fact[n] * fact[n] % MOD;
+        
+        long long combinations = numerator * power(denominator, MOD - 2) % MOD;
+        
+        return combinations * power(n + 1, MOD - 2) % MOD;
+    }
+};
